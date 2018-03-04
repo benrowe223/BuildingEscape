f06fcc73aecf721e20b27602c8449a683b07c656
ben.rowe <benrowe223@gmail.com>
2018-03-04 21:57:30 +0000

    Room built added doors and trigger volume

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
@@ -18,7 +18,12 @@ UPositionReporter::UPositionReporter()
 void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting..."));
+
+	FString objectName = GetOwner() -> GetName();
+	FString ObjectPosition = GetOwner() -> GetActorLocation().ToString();
+	
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *objectName, *ObjectPosition);
 	// ...
 	
 }
